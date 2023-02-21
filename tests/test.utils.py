5b7f93ab0565b2ae0e4b6ccc944bf71842265dec
askip <a.skiper2013@yandex.ru>
2023-02-21 22:12:45 +0300

    add tests

---
+++ tests/test.utils.py
@@ -0,0 +1,22 @@
+import unittest
+from utils.utils import Node, Stack
+
+class TestNode(unittest.TestCase):
+     def test_node(self):
+         node = Node(1, None)
+         self.assertEqual(node.data, 1)
+         self.assertEqual(node.next_node, None)
+
+     def test_node_next_node(self):
+         node1 = Node(1, None)
+         node2 = Node(2, node1)
+         self.assertEqual(node2.next_node, node1)
+         self.assertEqual(node2.next_node.data, 1)
+
+class TestStack(unittest.TestCase):
+     def test_stack_push(self):
+         stack = Stack()
+         stack.push('data_1', None)
+         stack.push('data_2', None)
+         self.assertEqual(stack.top.data, 'data_2')
+         self.assertEqual(stack.top.next_node.data, 'data_1')
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,4 @@
 class Node:
-    """ Это узел """
 
     def __init__(self, data, next_node):
         self.data = data  # тут данные
@@ -11,8 +10,6 @@ class Stack:
         self.top = None
 
     def push(self, data, next_node):
-        """ Тут добавление элемента """
         new_node = Node(data, next_node)
         new_node.next_node = self.top
         self.top = new_node
-
